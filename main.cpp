0ed5627a93af1e750bc5d63062b0dd4dbe8f669f
ddeexxiikk <jakub.batycki.jr@gmail.com>
2021-02-12 14:33:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,57 +10,56 @@ int losowanie(){
     // 3 - Nozyce
     int tab[3]={1,2,3};
     srand(time(0));
-    return tab[rand() % 3];
+    return tab[rand()%3];
 }
 
 int main()
 {
     int wybor_gracza, i_g=0, i_k=0;
-    int wybor_los=losowanie();
     cout << "Gramy do trzech zwyciestw." << endl;
 
     while(i_g<3 && i_k<3){
     LOOP:
     cout << "Podaj numer co wybierasz: 1 - Papier; 2 - Kamien; 3 - Nozyce? : ";
     cin >> wybor_gracza;
-    if(wybor_gracza>3){
+    if(wybor_gracza>3 || wybor_gracza<1){
         cout << "Zbyt duza liczba. Jeszcze raz!" << endl;
         goto LOOP;
     }
         if(wybor_gracza==1){
-            if(wybor_los==1)
+            if(losowanie()==1)
                 cout << "Remis!" << endl;
-            else if(wybor_los==2){
+            else if(losowanie()==2){
                 cout << "Wygrales!" << endl;
                 i_g++;
-                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl;
-            }else if(wybor_los==3){
+                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl << endl;
+            }else if(losowanie()==3){
                 cout << "Przegrales!" << endl;
                 i_k++;
-                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl;
+                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl << endl;
             }
         }else if(wybor_gracza==2){
-            if(wybor_los==1){
+            if(losowanie()==1){
                 cout << "Przegrales!" << endl;
                 i_k++;
-                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl;
-            }else if(wybor_los==2)
+                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl << endl;
+            }else if(losowanie()==2)
                 cout << "Remis!" << endl;
-            else if(wybor_los==3){
+            else if(losowanie()==3){
                 cout << "Wygrales!" << endl;
                 i_g++;
-                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl;
+                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl << endl;
             }
         }else{
-            if(wybor_los==1){
+            if(losowanie()==1){
                 cout << "Wygrales!" << endl;
                 i_g++;
-                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl;
-            }else if(wybor_los==2){
+                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl << endl;
+            }else if(losowanie()==2){
                 cout << "Przegrales!" << endl;
                 i_k++;
-                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl;
-            }else if(wybor_los==3)
+                cout << "Gracz - " << i_g << " : " << i_k << " - Komputer" << endl << endl;
+            }else if(losowanie()==3)
                 cout << "Remis!" << endl;
         }
     }
